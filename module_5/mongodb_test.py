7550777debd4d937e4f5b3b13c5da4f7b4bef5e1
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-15 21:37:40 -0600

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,26 +1,25 @@
-""" 
-    Title: mongodb_test.py
-    Author: Professor Krasso
-    Date: 10 July 2020
-    Description: Test program for connecting to a 
-                 MongoDB Atlas cluster
-"""
-
-""" import statements """
+#Importing pymongo and MongoClient
 from pymongo import MongoClient
-
-# MongoDB connection string 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
-
-# connect to the MongoDB cluster 
-client = MongoClient(url)
-
-# connect pytech database
-db = client.pytech
-
-# show the connected collections 
-print("\n -- Pytech COllection List --")
-print(db.list_collection_names())
-
-# show an exit message
-input("\n\n  End of program, press any key to exit... ")
+#Defining "insert_one()" command
+def insert_one():
+    #Establishing the connection to MongoDB
+    connect=MongoClient('mongodb+srv://admin:admin@cluster0.za5b2oe.mongodb.net/?retryWrites=true&w=majority')
+    #Creating/Using the "pytech" database
+    db=connect["pytech"]
+    #Creating/Using the "students" collection
+    collection=db["students"]
+    #Creating the entries for the collection
+    Student1 = {"first_name": "John", "last_name": "Lennon", "student_id": 1007}
+    Student2 = {"first_name": "Paul", "last_name": "McCartney", "student_id": 1008}
+    Student3 = {"first_name": "Ringo", "last_name": "Starr", "student_id": 1009}
+    #Establishing the code to combine the collection
+    Student1_student_id = collection.insert_one(Student1).inserted_id
+    Student2_student_id = collection.insert_one(Student2).inserted_id
+    Student3_student_id = collection.insert_one(Student3).inserted_id
+    #Printing the completed actions
+    print("Inserted student record into the students collection with document_id" + str(Student1_student_id))
+    print("Inserted student record into the students collection with document_id" + str(Student2_student_id))
+    print("Inserted student record into the students collection with document_id" + str(Student3_student_id))
+    print("""
+End of program, press any key to continue...""")
+insert_one()
