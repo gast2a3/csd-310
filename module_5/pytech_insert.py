7d62aa3de5d80202b9d01e0afceb3c84e05b74a5
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-15 21:39:01 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,124 +1,25 @@
-""" 
-    Title: pytech_insert.py
-    Author: Professor Krasso
-    Date: 10 July 2020
-    Description: Test program for inserting new documents 
-                 into the students collection 
-"""
-
-""" import statements """
+#Importing pymongo and MongoClient
 from pymongo import MongoClient
-
-# MongoDB connection string 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
-
-# connect to the MongoDB cluster 
-client = MongoClient(url)
-
-# connect pytech database
-db = client.pytech
-
-""" three student documents"""
-# Thorin Oakenshield's data document 
-thorin = {
-    "student_id": "1007",
-    "first_name": "Thorin",
-    "last_name": "Oakenshield",
-    "enrollments": [
-        {
-            "term": "Session 2",
-            "gpa": "4.0",
-            "start_date": "July 10, 2020",
-            "end_date": "September 14, 2020",
-            "courses": [
-                {
-                    "course_id": "CSD310",
-                    "description": "Database Development and Use",
-                    "instructor": "Professor Krasso",
-                    "grade": "A+"
-                },
-                {
-                    "course_id": "CSD320",
-                    "description": "Programming with Java",
-                    "instructor": "Professor Krasso",
-                    "grade": "A+"
-                }
-            ]
-        }
-    ]
-
-}
-
-# Bilbo Baggins data document 
-bilbo = {
-    "student_id": "1008",
-    "first_name": "Bilbo",
-    "last_name": "Baggins",
-    "enrollments": [
-        {
-            "term": "Session 2",
-            "gpa": "3.52",
-            "start_date": "July 10, 2020",
-            "end_date": "September 14, 2020",
-            "courses": [
-                {
-                    "course_id": "CSD310",
-                    "description": "Database Development and Use",
-                    "instructor": "Professor Krasso",
-                    "grade": "B+"
-                },
-                {
-                    "course_id": "CSD320",
-                    "description": "Programming with Java",
-                    "instructor": "Professor Krasso",
-                    "grade": "A-"
-                }
-            ]
-        }
-    ]
-}
-
-# Frodo Baggins data document
-frodo = {
-    "student_id": "1009",
-    "first_name": "Frodo",
-    "last_name": "Baggins",
-    "enrollments": [
-        {
-            "term": "Session 2",
-            "gpa": "1.5",
-            "start_date": "July 10, 2020",
-            "end_date": "September 14, 2020",
-            "courses": [
-                {
-                    "course_id": "CSD310",
-                    "description": "Database Development and Use",
-                    "instructor": "Professor Krasso",
-                    "grade": "C"
-                },
-                {
-                    "course_id": "CSD 320",
-                    "description": "Programming with Java",
-                    "instructor": "Professor Krasso",
-                    "grade": "B"
-                }
-            ]
-        }
-    ]
-}
-
-# get the students collection 
-students = db.students
-
-# insert statements with output 
-print("\n  -- INSERT STATEMENTS --")
-thorin_student_id = students.insert_one(thorin).inserted_id
-print("  Inserted student record Thorin Oakenshield into the students collection with document_id " + str(thorin_student_id))
-
-bilbo_student_id = students.insert_one(bilbo).inserted_id
-print("  Inserted student record Bilbo Baggins into the students collection with document_id " + str(bilbo_student_id))
-
-frodo_student_id = students.insert_one(frodo).inserted_id
-print("  Inserted student record Frodo Baggins into the students collection with document_id " + str(frodo_student_id))
-
-input("\n\n  End of program, press any key to exit... ")
+#Defining "insert_one()" command
+def insert_one():
+    #Establishing the connection to MongoDB
+    connect=MongoClient('mongodb+srv://admin:admin@cluster0.za5b2oe.mongodb.net/?retryWrites=true&w=majority')
+    #Creating/Using the "pytech" database
+    db=connect["pytech"]
+    #Creating/Using the "students" collection
+    collection=db["students"]
+    #Creating the entries for the collection
+    Student1 = {"first_name": "John", "last_name": "Lennon", "student_id": 1007}
+    Student2 = {"first_name": "Paul", "last_name": "McCartney", "student_id": 1008}
+    Student3 = {"first_name": "Ringo", "last_name": "Starr", "student_id": 1009}
+    #Establishing the code to combine the collection
+    Student1_student_id = collection.insert_one(Student1).inserted_id
+    Student2_student_id = collection.insert_one(Student2).inserted_id
+    Student3_student_id = collection.insert_one(Student3).inserted_id
+    #Printing the completed actions
+    print("Inserted student record into the students collection with document_id" + str(Student1_student_id))
+    print("Inserted student record into the students collection with document_id" + str(Student2_student_id))
+    print("Inserted student record into the students collection with document_id" + str(Student3_student_id))
+    print("""
+End of program, press any key to continue...""")
+insert_one()
