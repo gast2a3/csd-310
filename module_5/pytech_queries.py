3ffc02351fd47dbe67098b4982c79915e3f4ce6e
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-15 22:15:32 -0600

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -4,3 +4,7 @@ pytechdatabase=connection["pytech"]
 studentCollection=pytechdatabase["students"]
 print("DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY:")
 print(studentCollection.find_one({"student_id": 1008}))
+
+print("DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY:")
+for document in studentCollection.find():
+    print(document)
