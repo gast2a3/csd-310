168f7141bbf46e10bb1b1004af4f5a3c9e575d65
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-15 21:47:57 -0600

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 connection=MongoClient('mongodb+srv://admin:admin@cluster0.za5b2oe.mongodb.net/?retryWrites=true&w=majority')
 pytechdatabase=connection["pytech"]
 studentCollection=pytechdatabase["students"]
-print("The student is:")
+print("DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY:")
 print(studentCollection.find_one({"student_id": 1008}))
