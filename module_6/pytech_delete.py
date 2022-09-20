2cd8264ff465ebb331b6a4d3d73010c652772e06
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-20 12:13:51 -0600

    Update pytech_delete.py

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -6,7 +6,17 @@ def delete_one():
     print("\n DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY:\n")
     for document in studentCollection.find():
         print(document)
-    myQuery = {"first_name": "John"}
+    Student10 = {"first_name": "George", "last_name": "Harrison", "student_id": 1010}
+    #Establishing the code to combine the collection
+    Student10_student_id = studentCollection.insert_one(Student10).inserted_id
+    print("\n-- INSERT STATEMENTS --")
+    print("Inserted student record into the students collection with document_id" + str(Student10_student_id))
+    print("\n-- DISPLAYING STUDENT TEST DOC --")
+    print(studentCollection.find_one({"student_id": 1010}))
+    myQuery = {"first_name": "George"}
     studentCollection.delete_one(myQuery)
-    print(document)
+    print("\n DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY:\n")
+    for document in studentCollection.find():
+        print(document)
+    print("\nEnd of program, press any key to continue...\n")
 delete_one()
