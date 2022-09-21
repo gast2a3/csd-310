2b9006749aba4d6045205f42bec1b32f9a199a67
gast2a3 <95460357+gast2a3@users.noreply.github.com>
2022-09-21 16:26:22 -0600

    Update mysql_test.py

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -1,46 +1,32 @@
-""" 
-    Title: pysports_join_queries.py
-    Author: Professor Krasso
-    Date: 16 July 2020
-    Description: Test program for joining the player and team tables
-"""
-
-""" import statements """
+#Importing mysql.connector and errorcode
 import mysql.connector
 from mysql.connector import errorcode
-
-""" database config object """
+from pymongo import MongoClient
+#Establishing the connection to MongoDB
+connect=MongoClient('mongodb+srv://admin:admin@cluster0.za5b2oe.mongodb.net/?retryWrites=true&w=majority')
+#Creating/Using the "pytech" database
+db=connect["pytech"]
+#Building the config database object
 config = {
     "user": "pysports_user",
-    "password": "MySQL8IsGreat!",
-    "host": "127.0.0.1",
+    "password": "!QAZ1qaz@WSX2wsx",
+    "host": "localhost",
     "database": "pysports",
     "raise_on_warnings": True
 }
-
+#Establishing the connection and Displaying connection responses/errors
 try:
-    """ try/catch block for handling potential MySQL database errors """ 
-
-    db = mysql.connector.connect(**config) # connect to the pysports database 
-    
-    # output the connection status 
-    print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
-
-    input("\n\n  Press any key to continue...")
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {}".format(config["user"], config["host"], config["database"]))
+    input("\n\n Press any key to continue...")
 
 except mysql.connector.Error as err:
-    """ on error code """
-
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("  The supplied username or password are invalid")
+        print(" The supplied username or password are invalid")
 
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("  The specified database does not exist")
+        print(" The specified database does not exist")
 
     else:
         print(err)
 
-finally:
-    """ close the connection to MySQL """
-
-    db.close()
